607074c194ff0edbf77434d2e517b34489783cad
John Kang <JK@Johns-MacBook-Pro-2.local>
2019-01-25 02:09:43 -0600

    HW1 complete

--- student_code.py
+++ student_code.py
@@ -23,8 +23,15 @@ class KnowledgeBase(object):
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
+
+        if isinstance(fact,Fact) and not (fact in self.facts):
+            self.facts.append(fact)
+            return
+        elif isinstance(fact,Rule) and not (fact in self.rules):
+            self.rules.append(fact)
+            return
         print("Asserting {!r}".format(fact))
-        
+
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
 
@@ -34,4 +41,22 @@ class KnowledgeBase(object):
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
+        if isinstance(fact,Fact):
+            list_of_bindings = ListOfBindings()
+            for f in self.facts:
+                bindings = match(fact.statement,f.statement)
+                if bindings:
+                    list_of_bindings.add_bindings(bindings,[fact,f])
+                    # print("000000000")
+                    # print(f.statement)
+                    # print(fact.statement)
+                    # print(bindings)
+                    # # print(list_of_bindings)
+            if not len(list_of_bindings.list_of_bindings) == 0:
+                return list_of_bindings
+            else:
+                return False
+
+
         print("Asking {!r}".format(fact))
+
